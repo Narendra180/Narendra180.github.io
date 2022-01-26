b2ae5bdce739c6cc561035f6e0302f757b0e43ab
Narendra <narendrakanigiri18@gmail.com>
2022-01-27 01:07:07 +0530

    fixed animations

--- chatbox-script.js
+++ chatbox-script.js
@@ -50,31 +50,16 @@ let startANewConversationBtn =
                         document.querySelector(".start-a-new-conversation-btn");
 
 
-const setShowEle = (...eleWhereShowEleNeedToBeSet) => {
-    console.log(eleWhereShowEleNeedToBeSet);
-    let setShow = () => {
-        eleWhereShowEleNeedToBeSet.forEach((ele) => {
-            ele.classList.add("show-ele");
-        })
-    }
-    return () => {
-        setTimeout(setShow,1);
-    }
-}
-
-setShowEle(introductoryContentBackgroundImage)();
-
-introductoryContentBackgroundImage.ontransitionend = setShowEle(introductoryContentDiv);
+introductoryContentBackgroundImage.classList.add("show-ele");
+introductoryContentDiv.classList.add("show-ele");
 robotCircularBtn0.classList.add("show-ele");
+faqCon.classList.add("show-ele");
+robotCircularBtn.classList.add("show-ele");
+faqH3.classList.add("show-ele");
+questionAndAnswersListLis[0].classList.add("show-ele");
+questionAndAnswersListLis[1].classList.add("show-ele"); 
+questionAndAnswersListLis[2].classList.add("show-ele"); 
+questionAndAnswersListLis[3].classList.add("show-ele"); 
+startANewConversationBtn.classList.add("show-ele");
 
-introductoryContentDiv.ontransitionend = setShowEle(faqCon, robotCircularBtn);
-
-faqCon.ontransitionend = setShowEle(faqH3);
-
-faqH3.ontransitionend = setShowEle(questionAndAnswersListLis[0]);
-questionAndAnswersListLis[0].ontransitionend = setShowEle(questionAndAnswersListLis[1]);
-questionAndAnswersListLis[1].ontransitionend = setShowEle(questionAndAnswersListLis[2]);
-questionAndAnswersListLis[2].ontransitionend = setShowEle(questionAndAnswersListLis[3]);
-
-questionAndAnswersListLis[3].ontransitionend = setShowEle(startANewConversationBtn);
 
